540a655cab77ee1ece92e3314d6df0c1bc3e97df
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-21 22:29:05 -0700

    Implement error-rendering logic for login and signup forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to '/signup'
+      render :new
     end
-  end
+  end  
 
   private
 
